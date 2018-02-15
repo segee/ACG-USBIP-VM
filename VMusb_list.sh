ccfda15677fb6189ebb8bf71d500b9679814e8a7
Bruce Segee <segee@maine.edu>
2018-02-15 09:39:53 -0500

    First pass as VMusb_list.sh

---
+++ VMusb_list.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This gets the low byte of the machine that is connected to me
+# via xrdp.  There is probably a more elegant solution.
+# This will only work if xrdp is used.
+
+
+MYGUAC="192.168.0.`netstat -t |grep 3389 |cut -f 2 -d : |cut -f 4 -d .`"
+echo "my GUAC server is "$MYGUAC
+MYUSBPORT=$((6000+$1))
+MYSSHPORT=$((7000+$1))
+echo $MYUSBPORT
+sudo usbip --tcp-port $MYUSBPORT list -r $MYGUAC
